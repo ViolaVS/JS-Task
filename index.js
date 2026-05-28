464f7e24f5526430d43259c222fc741e35819441
satrija87 <satrija87@gmail.com>
2026-05-28 19:09:16 +0200

    feat: solved solution of implementing classes

---
+++ index.js
@@ -0,0 +1,147 @@
+class Rocket {
+  #fuelLevel = 100;
+
+  constructor(name, payloadCapacityKg, reusable) {
+    this.name = name;
+    this.payloadCapacityKg = payloadCapacityKg;
+    this.reusable = reusable;
+  }
+
+  refuel() {
+    this.#fuelLevel = 100;
+  }
+
+  consumeFuel(amount) {
+    if (this.#fuelLevel - amount < 0) {
+      const missingFuel = amount - this.#fuelLevel;
+      throw new Error(`Not enough fuel in the tank. Missing ${missingFuel} fuel.`);
+    }
+
+    if (amount <= 0) {
+      throw new Error('Consuming amout must be more then 0');
+    }
+
+    this.#fuelLevel -= amount;
+  }
+
+  isReadyToLaunch() {
+    return this.#fuelLevel >= 80;
+  }
+
+  toString() {
+    return `🚀 ${this.name} | Capacity: ${this.payloadCapacityKg}kg | Reusable: ${this.reusable ? 'Yes' : 'No'} | Fuel: ${this.#fuelLevel}%`;
+  }
+}
+
+class Crew {
+  #members = [];
+
+  constructor(maxSize = 7) {
+    this.maxSize = maxSize;
+  }
+  
+  addMember({ name, role }) {
+    if (this.#members.length >= this.maxSize) {
+      throw new Error(`Crew is full!`);
+    }
+    this.#members.push({ name, role });
+  }
+
+  get membersAmount() {
+    return this.#members.length;
+  }
+
+  hasCommander() {
+    return this.#members.some((member) => member.role === 'Commander');
+  }
+
+  isReady() {
+    return this.#members.length >= 1 && this.hasCommander();
+  }
+
+  getManifest() {
+    let title = `=== Crew (${this.#members.length}/${this.maxSize}) ===`;
+    let membersList = this.#members
+      .map((member, index) => `\n ${index + 1}. ${member.name} - ${member.role}`)
+      .join('');
+
+    return `${title}${membersList}`;
+  }
+}
+
+class Mission {
+  #status = 'planned';
+  static #counter = 1;
+
+  constructor(name, rocket, payloadKg) {
+    this.name = name;
+    this.rocket = rocket;
+    this.payloadKg = payloadKg;
+    this.id = Mission.generateId();
+  }
+
+  set status(newStatus) {
+    this.#status = newStatus;
+  }
+
+  get status() {
+    return this.#status;
+  }
+  static generateId() {
+    let id = `MSN-${String(Mission.#counter).padStart(3, '0')}`;
+    Mission.#counter++;
+
+    return id;
+  }
+
+  validate() {
+    let extraKg = this.payloadKg - this.rocket.payloadCapacityKg;
+
+    if (!this.rocket.isReadyToLaunch()) {
+      throw new Error(`${this.rocket.name} is not ready to launch. Please refuel.`);
+    }
+
+    if (this.payloadKg > this.rocket.payloadCapacityKg) {
+      throw new Error(`Payload for ${this.rocket.name} exceeds capacity by ${extraKg}kg`);
+    }
+  }
+
+  launch() {
+    this.validate();
+    this.rocket.consumeFuel(80);
+    let isSuccess = Math.random() < 0.7;
+
+    this.#status = isSuccess ? 'success' : 'failed';
+
+    return isSuccess
+      ? `✅ ${this.id} ${this.name} launched successfully!`
+      : `💥 ${this.id} ${this.name} failed on launch. `;
+  }
+
+  toString() {
+    return ` ${this.id} '${this.name}' | Status: ${this.#status}`;
+  }
+}
+
+class CrewedMission extends Mission {
+  constructor(name, rocket, payloadKg, crew) {
+    super(name, rocket, payloadKg);
+    this.crew = crew;
+  }
+
+  validate() {
+    super.validate();
+
+    if (this.crew.membersAmount < 1) {
+      throw new Error('Crew is not ready: no members assigned');
+    }
+
+    if (!this.crew.hasCommander()) {
+      throw new Error('Crew is not ready: no Commander assigned');
+    }
+  }
+
+  toString() {
+    return `${this.id} '${this.name}' | Status: ${this.status} | Crew: ${this.crew.membersAmount}`;
+  }
+}
